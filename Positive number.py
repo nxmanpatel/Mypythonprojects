d0f59a450455f87511c30aaf03af655c39bbcf9d
nxmanpatel <120243685+nxmanpatel@users.noreply.github.com>
2023-02-07 17:25:02 +0530

    Add files via upload

---
+++ Positive number.py
@@ -0,0 +1,17 @@
+
+print("List1:")
+list1 = (12,-7,5,64,-14)
+num = 0
+while (num < len(list1)):
+    if list1[num] >= 0 :
+        print(list1[num], end=" ")
+    num += 1
+print("           ")
+print("List2:")
+list2 = (12,14,-95,3)
+num = 0
+while (num < len(list2)):
+    if list2[num] >= 0 :
+        print(list2[num], end=" ")
+    num += 1
+
